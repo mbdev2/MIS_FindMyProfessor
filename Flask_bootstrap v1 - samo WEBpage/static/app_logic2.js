d902c8a1ac4884fe3b7967d36b6fa8b97eb7ea74
BP4769 <blaz.pridgar@gmail.com>
2021-05-06 11:28:20 +0200

    NE GRE FEKIN CHARTA NAREIT
    NE GRE FEKIN CHARTA NAREIT

--- Flask_bootstrap v1 - samo WEBpage/static/app_logic2.js
+++ Flask_bootstrap v1 - samo WEBpage/static/app_logic2.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     //connect to the socket server.
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
@@ -7,31 +8,33 @@ $(document).ready(function(){
       console.log(msg);
         console.log("Received number" + msg.number[0]);
         console.log("Received number" + msg.number[1]);
-        if (msg.number[0] > msg.number[1]){
-          number_string =
-          '<div class="row justify-content-center">'+
-          '<div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
-          '<img src="static/tabla.jpg" alt="Tabla" style="border:5px solid white" class="img-fluid">' +
-          '</div>'+
-          '<div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
-          //'<h3>Naprava 1: </h3>'+'<p>' + msg.number[0].toString() + '</p>'+ '</br>' +
-          '<img src="static/tabla2.jpg" alt="Tabla" class="img-fluid" style="opacity: 0.7">' +
-          //'<h3>Naprava 2: </h3>'+ '<p>' + msg.number[1].toString() + '</p>';
-          '</div>'+
-          '</div>'
-        }
-        else if (msg.number[0] < msg.number[1]){
-          number_string =
-          '<div class="row justify-content-center">'+
-          ' <div class="col-lg-6 col-md-6 col-sm-12"  style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
-          '<img src="static/tabla.jpg" alt="Tabla" class="img-fluid" style="opacity: 0.7">' +
-          '</div>'+
-          '<div class="col-lg-6 col-md-6 col-sm-12"  style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
-          //'<h3>Naprava 1: </h3>'+'<p>' + msg.number[0].toString() + '</p>'+ '</br>' +
-          '<img src="static/tabla2.jpg" alt="Tabla" style="border:5px solid white" class="img-fluid">' +
-          //'<h3>Naprava 2: </h3>'+ '<p>' + msg.number[1].toString() + '</p>';
-          '</div>'+
-          '</div>'
+        if (msg.number[0] > 60 || msg.number[1] > 60) {
+          if (msg.number[0] > msg.number[1]){
+            number_string =
+            '<div class="row justify-content-center">'+
+            '<div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
+            '<img src="static/tabla.jpg" alt="Tabla" style="border:5px solid white" class="img-fluid">' +
+            '</div>'+
+            '<div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
+            //'<h3>Naprava 1: </h3>'+'<p>' + msg.number[0].toString() + '</p>'+ '</br>' +
+            '<img src="static/tabla2.jpg" alt="Tabla" class="img-fluid" style="opacity: 0.7">' +
+            //'<h3>Naprava 2: </h3>'+ '<p>' + msg.number[1].toString() + '</p>';
+            '</div>'+
+            '</div>'
+          }
+          else if (msg.number[0] < msg.number[1]){
+            number_string =
+            '<div class="row justify-content-center">'+
+            ' <div class="col-lg-6 col-md-6 col-sm-12"  style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
+            '<img src="static/tabla.jpg" alt="Tabla" class="img-fluid" style="opacity: 0.7">' +
+            '</div>'+
+            '<div class="col-lg-6 col-md-6 col-sm-12"  style="padding-top: 40px; padding-right: 50px; padding-left: 50px">'+
+            //'<h3>Naprava 1: </h3>'+'<p>' + msg.number[0].toString() + '</p>'+ '</br>' +
+            '<img src="static/tabla2.jpg" alt="Tabla" style="border:5px solid white" class="img-fluid">' +
+            //'<h3>Naprava 2: </h3>'+ '<p>' + msg.number[1].toString() + '</p>';
+            '</div>'+
+            '</div>'
+          }
         }
         else{
           number_string =
@@ -47,5 +50,17 @@ $(document).ready(function(){
           '</div>'
         }
         $('#log').html(number_string);
+
+        char_string =
+        '<div class="row justify-content-center">'+
+        '<h2>Statistika uporabe:</h2>'+
+        '<h4>Leva Tabla: ' + msg.number[2].toString() + '</h4>'+
+        '<h4>Desna Tabla: ' + msg.number[3].toString() + '</h4>'+
+        '<h4>Ni pisanja: ' + msg.number[4].toString() + '</h4>'+
+        '</div>'
+
+        $('#char').html(char_string);
+
+
     });
 });
